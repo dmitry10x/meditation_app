2fe89a49e9255e071f82b132c79a2afe1971ed48
dmitry10x <fesfdi@gmail.com>
2020-03-29 15:41:42 +0300

    New function record_meditation - used to record a session a user did without the app

--- models.py
+++ models.py
@@ -38,14 +38,14 @@ class Meditation:
 
     def describe_session(self):
         duration = (self.time_end - self.time_start).total_seconds() // 60
-        print('Meditation №{}\nDuration: {} min.'.format(self.id, duration))
+        print('Meditation №{}\nDuration: {} min.'.format(self.id, int(duration)))
 
     def finish_meditation(self):
         msg = input("Time is up..\nEnter 'OK' when you are ready to finish your session: ")
         if msg.lower() == 'ok':
             self.time_end = datetime.datetime.now()
         else:
-            finish_meditation()
+            Meditation.finish_meditation(self)
 
     def ask_for_duration(self):
         duration_minutes = input('How long are you going to meditate? (min): ')
@@ -57,6 +57,16 @@ class Meditation:
         self.time_start = datetime.datetime.now()
         time.sleep(int(duration_minutes)*60)
         Meditation.finish_meditation(self)
+
+    def record_meditation(self, day_month_year, duration):
+        """
+        day_month_year has to be in format: 29.3.20 - 29th of March, 2020;
+        duration in minutes
+        """
+        day, month, year = day_month_year.split('.')
+        self.time_start = datetime.datetime(int(year), int(month), int(day))
+        self.time_end = self.time_start + datetime.timedelta(minutes=duration)
+        return self
 
     def __repr__(self):
         return '--> Meditation {}'.format(self.id, self.time_start, self.time_end)
